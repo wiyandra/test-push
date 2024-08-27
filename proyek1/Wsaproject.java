a73dd5022ecad2df494f1500f393a3699bbcc5ff
WiyandraSFL <wiyandrasfl@students.untidar.ac.id>
2024-08-27 21:39:17 +0700

    testpush

---
+++ proyek1/Wsaproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wsaproject;
+
+/**
+ *
+ * @author Alexigos
+ */
+public class Wsaproject {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO IM WIYANDRA FROM TIDAR UNIVERSITY");
+    }
+}
